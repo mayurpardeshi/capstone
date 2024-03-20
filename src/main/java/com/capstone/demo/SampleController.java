c1ae2ddc552012766ce6a8e87ec458d4d2f5acb3
mayurpardeshi <pardeshimayur0@gmail.com>
2024-03-20 15:22:06 +0530

    merged master

---
+++ src/main/java/com/capstone/demo/SampleController.java
@@ -0,0 +1,24 @@
+package com.capstone.demo;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greet")
+public class SampleController {
+    @GetMapping("/{name}")
+    public String sayHello(@PathVariable("name") String name){
+        return "Hello "+name;
+    }
+
+    public void fun(){
+
+    }
+
+    public void fun2(){
+
+    }
+}
