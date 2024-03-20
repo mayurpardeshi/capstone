77f625daee78cf6ed26fc5f95d72f158cb2e9da1
mayurpardeshi <pardeshimayur0@gmail.com>
2024-03-20 08:17:35 +0530

    Implementation of ProductService

---
+++ src/main/java/com/capstone/productservice/models/Product.java
@@ -0,0 +1,39 @@
+package com.capstone.productservice.models;
+
+public class Product {
+    private int id;
+    private String title;
+    private double price;
+    private String description;
+    private String category;
+    private String image;
+    private Rating rating;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+}
